5de75eb8df65c511d73dd4ea8b6128b8a8458646
FRANKRIMIRU <146768033+FRANKRIMIRU@users.noreply.github.com>
2025-01-02 11:58:01 +0300

    what is this

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "index.html",
+        "app.js", 
+  ],
+  theme: {
+    extend: {
+      colors: {
+        //customBlue: '#153777', // Old color
+        //customPurple: '#4e085f', // New color
+        //customBlueNew: '#002765',
+       // customGrey:'#edeef0',
+        //customWhite:'#fff',
+        //customOrange:'#ff5945'
+
+      },
+    },
+  },
+  plugins: [],
+}
+
